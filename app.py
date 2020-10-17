0383190a3c42388db1ea6eb36d6a77574e8fb8e9
SalmaQueen <salmawasee9@gmail.com>
2020-10-17 15:37:47 +0300

    made some changes

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from newsapi import NewsApiClient
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def index(name=None):
+    return render_template('index.html',name=name)
 
 
 @app.route('/news')
